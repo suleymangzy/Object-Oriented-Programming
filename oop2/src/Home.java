e45b5184865e48cd64bc61c1d76017aa3874edec
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-04-04 00:01:05 +0300

    Update Home.java

--- oop2/src/Home.java
+++ oop2/src/Home.java
@@ -1,10 +1,12 @@
 public class Home {
     private String street;
     private String number;
-
+   /*
+   The "this" keyword is used as "this.member_variable_name = local_variable_name" to distinguish between local variables 
+   and member variables with the same name created within a class in Java.
+   */
     public void setStreet(String street) {
         this.street = street;
-    }
 
     public void setNumber(String number) {
         this.number = number;
